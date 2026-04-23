b2e4b2fc2c2b16d1834d01df9eea30fe20a91bec
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2026-04-24 01:08:46 +0300

    test: добавил тест withExistingId_returnsNoContent

---
+++ src/test/ru/practicum/moviehub/http/MoviesDeleteTest.java
@@ -0,0 +1,65 @@
+package ru.practicum.moviehub.http;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoviesDeleteTest {
+    private static final String BASE = "http://localhost:8080";
+    private static MoviesServer server;
+    private static HttpClient client;
+    private static final String TEST_MOVIE = "{\"title\":\"Inception\",\"year\":2010}";
+
+    @BeforeAll
+    static void beforeAll() throws IOException, InterruptedException {
+        server = new MoviesServer();
+        server.start();
+
+        client = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(2))
+                .build();
+    }
+
+    @BeforeEach
+    void beforeEach() {
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        server.stop();
+    }
+
+    @Test
+    void deleteMovieById_withExistingId_returnsNoContent() throws Exception {
+        HttpRequest reqCreate = HttpRequest.newBuilder()
+                .uri(URI.create(BASE + "/movies"))
+                .headers("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(TEST_MOVIE, StandardCharsets.UTF_8))
+                .timeout(Duration.ofSeconds(2))
+                .build();
+
+        client.send(reqCreate, HttpResponse.BodyHandlers.ofString());
+
+        HttpRequest req = HttpRequest.newBuilder()
+                .uri(URI.create(BASE + "/movies/1"))
+                .DELETE()
+                .build();
+
+        HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(204, response.statusCode());
+        assertEquals("[]", server.store.getAll());
+    }
+}
