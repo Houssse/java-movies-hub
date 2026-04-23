503655c02c858f02375b59d1f2df391dc96a1150
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2026-04-23 20:44:08 +0300

    рефакторинг Handler

--- src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
+++ src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
@@ -10,8 +10,10 @@ public abstract class BaseHttpHandler implements HttpHandler {
     protected static final String CT_JSON = "application/json; charset=UTF-8";
 
     protected void sendJson(HttpExchange ex, int status, String json) throws IOException {
+        byte[] bytes = json.getBytes(StandardCharsets.UTF_8);
         ex.getResponseHeaders().set("Content-Type", CT_JSON);
-        ex.sendResponseHeaders(status, json.getBytes(StandardCharsets.UTF_8).length);
+        ex.sendResponseHeaders(status, bytes.length);
+        ex.getResponseBody().write(bytes);
     }
 
     protected void sendNoContent(HttpExchange ex) throws java.io.IOException {
--- src/main/ru/practicum/moviehub/http/MoviesHandler.java
+++ src/main/ru/practicum/moviehub/http/MoviesHandler.java
@@ -1,8 +1,6 @@
 package ru.practicum.moviehub.http;
 
-import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
-import ru.practicum.moviehub.model.Movie;
 import ru.practicum.moviehub.store.MoviesStore;
 
 import java.io.IOException;
@@ -20,21 +18,13 @@ public class MoviesHandler extends BaseHttpHandler{
     public void handle(HttpExchange ex) throws IOException {
         String method = ex.getRequestMethod();
         if (method.equalsIgnoreCase("GET")) {
-            byte[] bytes = "[]".getBytes(StandardCharsets.UTF_8);
             sendJson(ex, 200, "[]");
-            try (OutputStream os = ex.getResponseBody()){
-                os.write(bytes);
-            }
+
         } else if (method.equalsIgnoreCase("POST")) {
             String body = new String(ex.getRequestBody().readAllBytes());
             store.add(body);
 
-            byte[] bytes = "[]".getBytes(StandardCharsets.UTF_8);
-            ex.getResponseHeaders().set("Content-Type", "application/json");
             sendJson(ex, 201, "[]");
-            try (OutputStream os = ex.getResponseBody()) {
-                os.write(bytes);
-            }
         }
     }
 }
